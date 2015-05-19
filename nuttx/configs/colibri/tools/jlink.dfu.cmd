9c3cdd89d164e4dc7795eb31e8f57fc762a01d95
Librae <librae8226@gmail.com>
2015-05-19 15:24:35 +0800

    colibri: do some rename to avoid confusion

--- nuttx/configs/colibri/tools/jlink.dfu.cmd
+++ nuttx/configs/colibri/tools/jlink.dfu.cmd
@@ -3,6 +3,6 @@ si 1
 speed 4000
 r
 h
-loadbin nuttx.bin,0x08000000
+loadbin nuttx.bin,0x08010000
 r
 exit
--- nuttx/configs/colibri/tools/jlink.sh
+++ nuttx/configs/colibri/tools/jlink.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ is$1 = 'isrecovery' ]; then
-	JLinkExe -CommanderScript configs/colibri/tools/jlink.recovery.cmd
+if [ is$1 = 'isdfu' ]; then
+	JLinkExe -CommanderScript configs/colibri/tools/jlink.dfu.cmd
 else
 	JLinkExe -CommanderScript configs/colibri/tools/jlink.cmd
 fi
