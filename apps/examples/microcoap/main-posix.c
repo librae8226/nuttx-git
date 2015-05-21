7ec0ec52bdc1467886b1d8b72a4a16521be8c641
Librae <librae8226@gmail.com>
2015-05-21 14:37:40 +0800

    apps: coap: add an example for CoAP
    Ported from:
        https://github.com/1248/microcoap
    
    Snapshot:
    
        commit 200a0623de5d53a28afe7fb3ae05eb637b50ee0c
        Merge: 152e980 d493ffa
        Author: Toby Jaffey <jrt@hodgepig.org>
        Date:   Thu Mar 26 15:39:04 2015 +0000
    
        Merge pull request #22 from xgilles/master
    
        Add IPV6 support for main-posix

---
+++ apps/examples/microcoap/main-posix.c
@@ -0,0 +1,104 @@
+#include <nuttx/config.h>
+
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+#include <debug.h>
+
+#include <net/if.h>
+#include <netinet/in.h>
+
+#include <nuttx/net/arp.h>
+#include <apps/netutils/netlib.h>
+
+#include "coap.h"
+
+#define PORT 5683
+#ifdef CONFIG_BUILD_KERNEL
+int main(int argc, FAR char *argv[])
+#else
+int coap_main(int argc, char *argv[])
+#endif
+{
+    int fd;
+#ifdef CONFIG_NET_IPv6
+    struct sockaddr_in6 servaddr, cliaddr;
+#else /* IPV6 */
+    struct sockaddr_in servaddr, cliaddr;
+#endif /* IPV6 */
+    uint8_t buf[4096];
+    uint8_t scratch_raw[4096];
+    coap_rw_buffer_t scratch_buf = {scratch_raw, sizeof(scratch_raw)};
+
+    printf("Starting CoAP...\n");
+
+#ifdef CONFIG_NET_IPv6
+    fd = socket(AF_INET6,SOCK_DGRAM,0);
+#else /* IPV6 */
+    fd = socket(AF_INET,SOCK_DGRAM,0);
+#endif /* IPV6 */
+
+    bzero(&servaddr,sizeof(servaddr));
+#ifdef CONFIG_NET_IPv6
+    servaddr.sin6_family = AF_INET6;
+    servaddr.sin6_addr = in6addr_any;
+    servaddr.sin6_port = htons(PORT);
+#else /* IPV6 */
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(PORT);
+#endif /* IPV6 */
+    bind(fd,(struct sockaddr *)&servaddr, sizeof(servaddr));
+
+    endpoint_setup();
+
+    while(1)
+    {
+        int n, rc;
+        socklen_t len = sizeof(cliaddr);
+        coap_packet_t pkt;
+
+        n = recvfrom(fd, buf, sizeof(buf), 0, (struct sockaddr *)&cliaddr, &len);
+#ifdef DEBUG
+        printf("Received: ");
+        coap_dump(buf, n, true);
+        printf("\n");
+#endif
+
+        if (0 != (rc = coap_parse(&pkt, buf, n)))
+            printf("Bad packet rc=%d\n", rc);
+        else
+        {
+            size_t rsplen = sizeof(buf);
+            coap_packet_t rsppkt;
+#ifdef DEBUG
+            coap_dumpPacket(&pkt);
+#endif
+            coap_handle_req(&scratch_buf, &pkt, &rsppkt);
+
+            if (0 != (rc = coap_build(buf, &rsplen, &rsppkt)))
+                printf("coap_build failed rc=%d\n", rc);
+            else
+            {
+#ifdef DEBUG
+                printf("Sending: ");
+                coap_dump(buf, rsplen, true);
+                printf("\n");
+#endif
+#ifdef DEBUG
+                coap_dumpPacket(&rsppkt);
+#endif
+
+                sendto(fd, buf, rsplen, 0, (struct sockaddr *)&cliaddr, sizeof(cliaddr));
+            }
+        }
+    }
+
+    return 0;
+}
